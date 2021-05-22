bc519fc92603220d2a1ab44f78f995cd25647ae9
Tamara Mansi <tamaramansi@yahoo.com>
2021-05-22 22:36:50 +0300

    Create HW 1.py

---
+++ HW code/HW 1.py
@@ -0,0 +1,6 @@
+# homework1 ( simple calculater) 
+n1 = 10
+n2 = 7
+print( "sum of n1+n2 ", n1+n2,sep="=")
+print( "multipliction of n1*n2 ", n1*n2,sep="=")
+print( "subtraction of n1-n2 ", n1-n2,sep="=") 
